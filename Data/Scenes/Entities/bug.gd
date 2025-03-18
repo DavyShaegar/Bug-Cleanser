84f0e0871876b225b4343c5fc59a4ba0500bf0c9
DavyShaegar <davy.shaegar@gmail.com>
2025-03-18 16:40:18 +0100

    Update
    Steam collision
    Bug basic navigation

--- Data/Scenes/Entities/bug.gd
+++ Data/Scenes/Entities/bug.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 class_name BugEnemy
 
 @export_category("Enemy Stats")
-@export var speed: float = 400.0
+@export var speed: float = 20.0
 @export var health: float = 10.0
 @export var rew_points: int = 0
 
@@ -11,18 +11,31 @@ class_name BugEnemy
 @onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
 @onready var player: Player
 
+func navigate(nav: NavigationAgent2D, new_speed, delta):
+	if !nav.is_navigation_finished():
+		var direction = (nav.get_next_path_position() - global_position).normalized()
+		
+		translate(direction * new_speed * delta)
+		look_at(direction)
+		move_and_slide()
+		
 func set_target(target: Vector2) -> void:
+	await get_tree().process_frame
 	nav_agent.target_position = target
+		
+func animate() -> void:
+	pass
+	#animation.play(states.find_key(current_state))
+	
+func moving(delta) -> void:
+	set_target(player.global_position)
+	navigate(nav_agent, speed, delta)
 
 func _ready() -> void:
 	player = get_node("../Player")
 	set_target(player.global_position)
 
 func _process(delta: float) -> void:
-	look_at(player.global_position)
-	
-	if nav_agent.is_navigation_finished():
-		return
+
+	moving(delta)
 		
-	nav_agent.velocity = global_position.direction_to(nav_agent.get_next_path_position())
-	move_and_slide()
--- Data/Scenes/Entities/player.gd
+++ Data/Scenes/Entities/player.gd
@@ -6,48 +6,38 @@ class_name Player
 @export var health: float = 100.0
 @export var score: int = 0
 
-enum States {
-				idle,
-				move,
-				shoot,
-				death 
-			}
-@onready var current_state: States = States.idle
 
 # Player nodes
 @onready var steam: GPUParticles2D = %Steam
 @onready var gun_node: Node2D = $Gun
 
-func change_state(new_state: States) -> void:
-	if current_state == 2:
-		return
-	current_state = new_state
+# Other variables
+@onready var mouse_pos: Vector2
+
+func rotate_gun(mouse_pos: Vector2) -> void:
+	var centre: Vector2 = global_position
+	var angle: float = (mouse_pos - centre).angle()
+	gun_node.position = Vector2(20,0).rotated(angle)
 	
 func shoot() -> void:
-	steam.emitting = true
-	change_state(States.shoot)
-
-	if Input.is_action_pressed("shoot") == false:
+	if Input.is_action_pressed("shoot"):
+		steam.emitting = true
+	elif Input.is_action_just_released("shoot"):
 		steam.emitting = false
-		change_state(States.idle)
 		
 func _physics_process(delta: float) -> void:
-	print(States.find_key(current_state))
-	gun_node.look_at(get_global_mouse_position())
-	
-	if Input.is_action_pressed("shoot"):
-		shoot()
-		
+	mouse_pos = get_global_mouse_position()
+	gun_node.look_at(mouse_pos)
+	rotate_gun(mouse_pos)
+	shoot()
 		
 	var direction := Input.get_axis("move_left", "move_right")
 	var y_dir := Input.get_axis("move_up", "move_down")
 	if direction or y_dir:
 		velocity.x = direction * (speed*10) * delta
 		velocity.y = y_dir * (speed*10) * delta
-		change_state(States.move)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed * delta)
 		velocity.y = move_toward(velocity.y, 0, speed * delta)
-		change_state(States.idle)
 
 	move_and_slide()
